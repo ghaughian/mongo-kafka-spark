87c6125cc3016aff6cd2bcd1e8b120aa1ed2c41c
ghaughian <gerardhaughian@gmail.com>
2017-02-18 17:09:01 +0000

    split aggregation into separate function

--- analytics_consumer_mongo.py
+++ analytics_consumer_mongo.py
@@ -26,35 +26,40 @@ c = Consumer({'bootstrap.servers': 'localhost',
               'group.id': 'mygroup',
               'default.topic.config': {'auto.offset.reset': 'smallest'}})
 c.subscribe(['topic_json'])
+                
 
-def agg():
-    msgs = []
-    i=0
-    # aggregate 10 messages at a time
-    while (i<10):
-        msg = c.poll()
-        if not msg.error():
-            print('Received message: %s' % msg.value().decode('utf-8'))
-            json_msg = json.loads(msg.value())
-            msgs.append(json_msg)
-        elif msg.error().code() != KafkaError._PARTITION_EOF:
-            print(msg.error())
-        if i==9:
-            df = pd.DataFrame(msgs) 
-            # very basic aggregation
-            aggDF = df.groupby("airline_id").count()
-            coll.insert_many(aggDF.to_dict('records'))
-            i=0
-            msgs=[]
-        else:
-            i=i+1
-    #c.close()
+def aggregation_basic(msgs):
+    df = pd.DataFrame(msgs) 
+    aggDF = df.groupby("airline_id").count()
+    coll.insert_many(aggDF.to_dict('records'))
+
+def consume():
+    try:
+        msgs = []
+        i=0
+        # aggregate 10 messages at a time
+        while (i<10):
+            msg = c.poll()
+            if not msg.error():
+                print('Received message: %s' % msg.value().decode('utf-8'))
+                msgs.append(json.loads(msg.value()))
+            elif msg.error().code() != KafkaError._PARTITION_EOF:
+                print(msg.error())
+            if i==9:
+                aggregation_basic(msgs)
+                i=0
+                msgs=[]
+            else:
+                i=i+1
+    finally:
+        c.close()
 
 def main():
-    agg()
+    consume()
 
 if __name__ == "__main__": main()
 
-# TODO: Have `agg` operate on a timer rather than x number of records e.g:
+# TODO: Add more complex aggregations
+# TODO: Have `consume` operate on a timer rather than x number of records e.g:
 # import threading
 # threading.Timer(1, ts).start()
